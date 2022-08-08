7c472db5a1464ccbfedb641c5b794c8349704f95
Ilia Kurganskii <ilya.kurganskii@gmail.com>
2022-08-08 17:57:49 +0200

    build: update terraform variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -8,10 +8,21 @@ variable "place" {
     sunset_time_h_utc = number
     sunset_time_m_utc = number
   })
+
+  default = {
+    name              = "hague"
+    displayName       = "Den Haag"
+    lat               = "52.117891"
+    lon               = "4.279861"
+    stream_url        = "https://59f185ece6219.streamlock.net/live/_definst_/sys2.stream/playlist.m3u8"
+    sunset_time_h_utc = 21
+    sunset_time_m_utc = 42
+  }
 }
 
 variable "recorder_image_version" {
-  type = string
+  type    = string
+  default = "latest"
 }
 
 variable "openweather_token" {
