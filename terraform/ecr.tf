754e2ae4867f2b5bf8e856094ed686eceb2174a2
Ilia Kurganskii <ilya.kurganskii@gmail.com>
2022-08-09 18:43:27 +0200

    feat(ecr): remove old images

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,3 +1,27 @@
 resource "aws_ecr_repository" "sunset_recorder" {
   name = "sunset_recorder"
 }
+
+resource "aws_ecr_lifecycle_policy" "foopolicy" {
+  repository = aws_ecr_repository.sunset_recorder.name
+
+  policy = jsondecode(
+    {
+      "rules" : [
+        {
+          "rulePriority" : 1,
+          "description" : "Expire images older than 2 days",
+          "selection" : {
+            "tagStatus" : "untagged",
+            "countType" : "sinceImagePushed",
+            "countUnit" : "days",
+            "countNumber" : 2
+          },
+          "action" : {
+            "type" : "expire"
+          }
+        }
+      ]
+    }
+  )
+}
