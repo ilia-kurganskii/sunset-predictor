df0c80a6d6408b1db8bb3a7a212969814525779f
Ilia Kurganskii <ilya.kurganskii@gmail.com>
2022-08-10 01:42:07 +0200

    build: refactor terraform infra

--- terraform/environments/dev/variables.tf
+++ terraform/environments/dev/variables.tf
@@ -9,6 +9,19 @@ variable "places" {
     sunset_time_h_utc = number
     sunset_time_m_utc = number
   }))
+
+  default = {
+    "hague" : {
+      id                = "hague"
+      name              = "hague"
+      displayName       = "Den Haag"
+      lat               = "52.117891"
+      lon               = "4.279861"
+      stream_url        = "https://59f185ece6219.streamlock.net/live/_definst_/sys2.stream/playlist.m3u8"
+      sunset_time_h_utc = 19
+      sunset_time_m_utc = 22
+    }
+  }
 }
 
 variable "recorder_image_version" {
