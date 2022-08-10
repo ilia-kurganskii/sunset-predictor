bd1b77fc071e0e2bd2fb566e9bc85bdf50573c1f
Ilia Kurganskii <ilya.kurganskii@gmail.com>
2022-08-11 00:17:04 +0200

    build: update timeout

---
+++ terraform/modules/services/telegram_bot_service/iam.tf
@@ -0,0 +1,48 @@
+resource "aws_iam_role" "lambda_exec" {
+  name = "${var.env}_serverless_lambda"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "lambda_access_resources_policy" {
+  name = "${var.env}_lambda_policy"
+  path = "/"
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "s3:GetObject",
+        ]
+        Effect   = "Allow"
+        Resource = [var.bucket_records_arn, "${var.bucket_records_arn}/*"]
+      },
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ],
+        "Resource" : "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_policy" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = aws_iam_policy.lambda_access_resources_policy.arn
+}
--- terraform/modules/services/telegram_bot_service/lambda.tf
+++ terraform/modules/services/telegram_bot_service/lambda.tf
@@ -9,6 +9,7 @@ resource "aws_lambda_function" "telegram_bot" {
   s3_bucket = aws_s3_bucket.telegram_bot_lambda_function.id
   s3_key    = data.aws_s3_object.lambda_zip.key
 
+  timeout = 15
   runtime = "nodejs16.x"
   handler = "dist/main.handler"
 
@@ -34,51 +35,3 @@ resource "aws_lambda_function_url" "lambda_url" {
   authorization_type = "NONE"
 }
 
-resource "aws_iam_role" "lambda_exec" {
-  name = "${var.env}_serverless_lambda"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Sid    = ""
-        Principal = {
-          Service = "lambda.amazonaws.com"
-        }
-      }
-    ]
-  })
-}
-
-resource "aws_iam_policy" "lambda_access_resources_policy" {
-  name = "${var.env}_lambda_policy"
-  path = "/"
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = [
-          "s3:GetObject",
-        ]
-        Effect   = "Allow"
-        Resource = [var.bucket_records_arn, "${var.bucket_records_arn}/*"]
-      },
-      {
-        "Effect" : "Allow",
-        "Action" : [
-          "logs:CreateLogGroup",
-          "logs:CreateLogStream",
-          "logs:PutLogEvents"
-        ],
-        "Resource" : "*"
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "lambda_policy" {
-  role       = aws_iam_role.lambda_exec.name
-  policy_arn = aws_iam_policy.lambda_access_resources_policy.arn
-}
