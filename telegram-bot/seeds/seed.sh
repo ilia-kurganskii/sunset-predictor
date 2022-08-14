2c5a3d8583a71fc0f1b4800b8967b410bb9493f1
Ilia Kurganskii <ilya.kurganskii@gmail.com>
2022-08-14 21:05:07 +0200

    feat: fix webhook url

--- telegram-bot/seeds/seed.sh
+++ telegram-bot/seeds/seed.sh
@@ -1,5 +1,5 @@
 LAMBDA_NAME=$1
-WEBHOOK_URL=$1
+WEBHOOK_URL=$2
 PLACES_DIR=$(dirname -- "$0")/places
 find $PLACES_DIR -maxdepth 1 -type f -exec \
   aws lambda invoke --cli-binary-format raw-in-base64-out --function-name "$LAMBDA_NAME" --payload file://{} ./response.json \; >>/dev/null
